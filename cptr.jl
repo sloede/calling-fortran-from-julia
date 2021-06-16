02689ad040c3d2679c2fe970c5aba72b31eeb624
Michael Schlottke-Lakemper <michael@sloede.com>
2021-06-16 22:48:46 +0200

    Add Julia script

---
+++ cptr.jl
@@ -0,0 +1,19 @@
+using Libdl: dlext
+
+# Platform-independent library name
+libcptr_path() = joinpath(".", "libcptr."*dlext)
+
+# Set array length to 4
+n = Cint(4)
+
+# Allocate object in Fortran
+cptr = ccall((:allocatestuff, libcptr_path()), Ptr{Cvoid}, (Ref{Cint},), n)
+
+# Print some information about it
+ccall((:enquirestuff, libcptr_path()), Cvoid, (Ref{Ptr{Cvoid}},), cptr)
+
+# Release memory
+ccall((:clearstuff, libcptr_path()), Cvoid, (Ref{Ptr{Cvoid}},), cptr)
+
+# Print pointer value
+println(" ", cptr)
